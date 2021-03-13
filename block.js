4d735f767718eeac2d540e159dcbf64958b81aba
sanjamveer1546 <72009838+sanjamveer1546@users.noreply.github.com>
2021-03-13 12:45:06 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,29 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          'isStatic':false
+      }
+     
+      
+      
+      this.width = width;
+      this.height = height;
+      
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      World.add(world, this.body);
+      
+      
+     }
+    
+    
+     display(){
+      
+      var pos =this.body.position;
+     //rectMode(CENTER)
+     rectMode(CENTER)
+   
+      rect(pos.x , pos.y, this.width, this.height);
+  
+     
+    }
+  };
